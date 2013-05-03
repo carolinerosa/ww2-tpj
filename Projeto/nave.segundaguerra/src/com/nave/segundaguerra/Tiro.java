0aa707c080e7367f6916e6aed51ba7b96bd0ff81
nunoduarte.nave <nunoduarte.nave@cb84909f-3d3d-d779-5d8c-9690d843c03f>
2013-05-03 20:30:11 +0000

    Implementação da instanciação do Tiro.

---
+++ Projeto/nave.segundaguerra/src/com/nave/segundaguerra/Tiro.java
@@ -0,0 +1,35 @@
+package com.example.tiro;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.Log;
+
+public class Tiro {
+	Bitmap tiro;
+	private float posicaoInicialY;
+	float velocidadeTiro;
+	float posicaoY, posicaoX;
+	public Tiro(Context context, float velocidade){
+		this.velocidadeTiro = velocidade;
+		try {
+			InputStream is = context.getAssets().open("quadros/monalisa.png");
+			tiro = BitmapFactory.decodeStream(is);
+		} catch (IOException e) {
+			Log.e(MainActivity.TAG, "Erro carregando imagem");
+		}
+	}
+	public void DrawTiro(Canvas canvas, Bitmap minhaArma, Paint paint){
+		posicaoX = minhaArma.getWidth();
+		posicaoY = minhaArma.getHeight() - posicaoInicialY;
+		canvas.drawBitmap(tiro, posicaoX, posicaoY, paint);
+	}
+	void update(){
+		posicaoInicialY += velocidadeTiro;
+	}
+}
