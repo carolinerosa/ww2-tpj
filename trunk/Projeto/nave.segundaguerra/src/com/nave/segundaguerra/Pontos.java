1fe0db453b47b98067a8e27be2f4a15aded6db86
carolinerosa.nave <carolinerosa.nave@cb84909f-3d3d-d779-5d8c-9690d843c03f>
2013-06-06 19:45:43 +0000

    Grupo Thyago - Classe pontos com métodos de perda e ganho.

---
+++ trunk/Projeto/nave.segundaguerra/src/com/nave/segundaguerra/Pontos.java
@@ -0,0 +1,23 @@
+package com.example.armas;
+
+
+public class Pontos{
+
+	private int points;
+	
+	public int Pontos(){
+		
+		return points;		
+	}
+	
+	public void Perda()
+	{
+		points-=1;
+	}
+	
+	public void Ganho()
+	{
+		points+=1;
+	}
+
+}
