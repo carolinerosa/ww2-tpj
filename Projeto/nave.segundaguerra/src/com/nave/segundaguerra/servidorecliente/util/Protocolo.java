72922b49e48c72bd10e4238d309061cc6e13e08d
gustavosilva.nave@gmail.com <gustavosilva.nave@gmail.com@cb84909f-3d3d-d779-5d8c-9690d843c03f>
2013-06-14 18:50:23 +0000

    Create Events in Server and Client. Change Protocol

--- Projeto/nave.segundaguerra/src/com/nave/segundaguerra/servidorecliente/util/Protocolo.java
+++ Projeto/nave.segundaguerra/src/com/nave/segundaguerra/servidorecliente/util/Protocolo.java
@@ -6,4 +6,10 @@ public interface Protocolo {
 	public static final String PROTOCOL_MOVE = "MOVE :";
 	public static final String PROTOCOL_UPDATE = "UPDATE :";
 	public static final String PROTOCOL_SHOOT = "SHOOT :";
+	public static final String PROTOCOL_RESPAWN = "RESPAWN :";
+	public static final String PROTOCOL_DAMAGE = "DAMAGE :";
+	public static final String PROTOCOL_DEATH = "DEATH :";
+	public static final String PROTOCOL_ITEM = "ITEM :";
+	public static final String PROTOCOL_ASKITEM = "ASKITEM :";
+	public static final String PROTOCOL_SCORE = "SCORE :";
 }
