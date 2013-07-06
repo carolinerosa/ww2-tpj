6b8d24fd3fc66e894866344ab76432f762859b0d
thaynanmoreira.nave@gmail.com <thaynanmoreira.nave@gmail.com@cb84909f-3d3d-d779-5d8c-9690d843c03f>
2013-07-06 22:21:03 +0000

    ViewPort mostra apenas o mapa(ele não mostra a parte branca)

--- Projeto/nave.segundaguerra/src/com/nave/segundaguerra/servidorecliente/cliente/ViewPort.java
+++ Projeto/nave.segundaguerra/src/com/nave/segundaguerra/servidorecliente/cliente/ViewPort.java
@@ -18,34 +18,50 @@ public class ViewPort {
 	int alturaTela, larguraTela;
 	JogadorCliente jogador;
 	Point posPlayerDraw;
+
+	private boolean SeguirPlayerX = false;
+	private boolean SeguirPlayerY = false;
+	private Paint paint;
+	
+	Point posMapa;
+	int mapaVerifyContX = 0;
+	int mapaVerifyContY = 0;
 	
 	public ViewPort(JogadorCliente playerCliente){
 		this.jogador = playerCliente;
+		paint = new Paint();
+		paint.setColor(Color.BLACK);
+		paint.setTextSize(20);
+		
+		int oldX = MapaCliente.getInstance().getX();
+		int oldY = MapaCliente.getInstance().getY();
+		
+		this.posMapa = new Point(oldX, oldY);
 		
 	}
 	
 	public void update(){
 		
-		
 		this.largura = this.x + this.larguraTela;
 		this.altura = this.y + this.alturaTela;
 		
+		checkMapa();
 	}
 	
-	public boolean checkDraw(int objetoX, int objetoY){
-		if(objetoX >= x){
+	public boolean checkDraw(int X, int Y){
+		if(X >= x){
 			return true;
 		}
 		
-		if(objetoX <= altura){
+		if(X <= altura){
 			return true;
 		}
 		
-		if(objetoY >= y){
+		if(Y >= y){
 			return true;
 		}
 		
-		if(objetoY <= largura){
+		if(Y <= largura){
 			return true;
 		}
 		
@@ -57,18 +73,32 @@ public class ViewPort {
 		int oldX = player.getX();
 		int oldY = player.getY();
 		
-		
-		this.x = jogador.getX() - (larguraTela/2);
-		this.y = jogador.getY() - (alturaTela/2);
-		
-		player.setX((oldX - x));
-		player.setY((oldY - y));
+		//Acompanha o Player
+		if(SeguirPlayerX)
+		{
+			this.x = jogador.getX() - (larguraTela/2);
+			
+		}
+		if(SeguirPlayerY)
+		{
+			this.y = jogador.getY() - (alturaTela/2);
+		}
 		
 		if(player.nome == jogador.nome)
 		{
 			posPlayerDraw = new Point(oldX - x, oldY - y);
 		}
-		player.draw(canvas);
+		
+		if(this.checkDraw(oldX - x, oldY - y))
+		{
+			player.setX((oldX - x));
+			player.setY((oldY - y));
+			
+			
+			player.draw(canvas);
+			canvas.drawText("<" + player.getNome() + ">", player.x - 30
+					, player.y + 30, paint);
+		}
 		
 		player.setX(oldX);
 		player.setY(oldY);
@@ -80,8 +110,11 @@ public class ViewPort {
 		Point old = tiro.getPosition();
 		Point next = new Point((old.x - x), (old.y - y));
 		
-		tiro.setPosition(next);
-		tiro.DrawTiro(canvas);
+		if(this.checkDraw(next.x, next.y))
+		{
+			tiro.setPosition(next);
+			tiro.DrawTiro(canvas);
+		}
 		
 		tiro.setPosition(old);
 
@@ -89,16 +122,16 @@ public class ViewPort {
 	
 	public void drawInViewPort(MapaCliente mapa, Canvas canvas)
 	{
-		int oldX = mapa.getX();
-		int oldY = mapa.getY();
+		int oldX = MapaCliente.getInstance().getX();
+		int oldY = MapaCliente.getInstance().getY();
 		
-		mapa.setX(oldX - x);
-		mapa.setY(oldY - y);
+		MapaCliente.setX(oldX - x);
+		MapaCliente.setY(oldY - y);
 		
-		mapa.Draw(canvas);
+		MapaCliente.getInstance().Draw(canvas);
 		
-		mapa.setX(oldX);
-		mapa.setY(oldY);
+		MapaCliente.setX(oldX);
+		MapaCliente.setY(oldY);
 		
 	}
 	
@@ -109,6 +142,67 @@ public class ViewPort {
 	public void setTela(int Altura, int Largura){
 		this.alturaTela = Altura;
 		this.larguraTela = Largura;
+	}
+	
+public void checkMapa(){
+		
+		MapaCliente map = MapaCliente.getInstance();
+		int centroTelaX = larguraTela/2;
+		int centroTelaY = alturaTela/2;
+		
+		
+		if(this.x <= posMapa.x){
+			SeguirPlayerX = false;
+			if(mapaVerifyContX == 0){
+				this.x = posMapa.x;
+				mapaVerifyContX = 1;
+			}
+			if(posPlayerDraw.x > centroTelaX){
+			SeguirPlayerX =true;
+			}
+		}else{
+			mapaVerifyContX = 0;
+		}
+		
+		if(this.y <= posMapa.y){
+			SeguirPlayerY = false;
+			if(mapaVerifyContY == 0){
+				this.y = posMapa.y;
+				mapaVerifyContY = 1;
+			}
+			if(posPlayerDraw.y > centroTelaY){
+			SeguirPlayerY = true;
+			}
+		}else{
+			mapaVerifyContY = 0;
+		}
+		
+		if(this.altura >=  map.getAltura()){
+			SeguirPlayerY = false;
+			if(mapaVerifyContY == 0){
+				this.y = map.getAltura() - alturaTela;
+				mapaVerifyContY = 1;
+			}
+			if(posPlayerDraw.y < centroTelaY){
+			SeguirPlayerY =true;
+			}
+		}else{
+			mapaVerifyContY = 0;
+		}
+		
+		if(this.largura >= map.getLargura()){
+			SeguirPlayerX = false;
+			if(mapaVerifyContX == 0){
+				this.x = map.getLargura() - larguraTela;
+				mapaVerifyContX = 1;
+			}
+			if(posPlayerDraw.x < centroTelaX){
+			SeguirPlayerX =true;
+			}
+		}else{
+			mapaVerifyContX = 0;
+		}
+		
 	}
 	
 }
