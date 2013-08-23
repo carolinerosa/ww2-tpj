49ca8b4622401eedc6a28aabd71152124ef49f82
pabloalexandre.nave@gmail.com <pabloalexandre.nave@gmail.com@cb84909f-3d3d-d779-5d8c-9690d843c03f>
2013-08-23 11:54:08 +0000

    Classes que estavam faltando

--- Projeto/nave.segundaguerra/src/com/nave/segundaguerra/servidorecliente/servidor/TiroServer.java
+++ Projeto/nave.segundaguerra/src/com/nave/segundaguerra/servidorecliente/servidor/TiroServer.java
@@ -3,82 +3,112 @@ package com.nave.segundaguerra.servidorecliente.servidor;
 import java.util.Iterator;
 import java.util.concurrent.ConcurrentHashMap;
 
+import android.content.Context;
 import android.graphics.Point;
+import android.graphics.PointF;
 import android.graphics.Rect;
+import android.graphics.RectF;
 import android.util.Log;
 
 import com.nave.segundaguerra.activitys.GerenciadorActivity;
+import com.nave.segundaguerra.game.Player;
+import com.nave.segundaguerra.servidorecliente.util.DialogHelper;
+import com.nave.segundaguerra.servidorecliente.util.RectLibrary;
 
-public class TiroServer {
-
+public class TiroServer 
+{
 	private Point posicaoTiro;
 	private Point velocidade;
 	private PlayerServer meuDono;
 	private Rect balaRect;
-	private int dano = 20;
 	
-	public TiroServer(PlayerServer player, Point touch){
+	public TiroServer(PlayerServer player, Point touch)
+	{
+		if(player == null)
+			Log.e("PlayerMissing", "Player Missing");
 		
 		this.posicaoTiro = player.getPosition();
-		//player.definirAngulo(touch);
+		
 		this.velocidade = new Point(touch.x/15, touch.y/15);
 		this.meuDono = player;
 		
-		balaRect = new Rect(GerenciadorActivity.GetInstance().getBalaRect());
+		balaRect = RectLibrary.getInstance().getRect("Projetil");
 		
 	}
 	
-	void update(){
+	void update()
+	{
 		posicaoTiro.x += this.velocidade.x; 
 		posicaoTiro.y += this.velocidade.y;
-		
+
 		this.setPosition(posicaoTiro);
-		//Log.i("Tiro", "Estou no update !!!");
 	}
 	
-	public boolean checarColisao(ConcurrentHashMap<String, JogadorServer> jogadores){
+	public boolean offsetScreen()
+	{
+		if(posicaoTiro.x > MapaServer.getLargura() || posicaoTiro.y > MapaServer.getAltura() || posicaoTiro.x < 0 || posicaoTiro.y < 0)
+			return true;
+		
+		return false;
+	}
+	
+	public boolean checarColisao(ConcurrentHashMap<String, JogadorServer> jogadores)
+	{
 
 		Iterator iterator = jogadores.keySet().iterator();
-		while (iterator.hasNext()) {
+		
+		while (iterator.hasNext()) 
+		{
 			String key = (String) iterator.next();
 			JogadorServer jogador = jogadores.get(key);
-			//Log.i("COLISAO TIRO", jogador.nome + jogador.toStringCSV());
+			
 			if(this.getOwner().getTime() != jogador.getTime())
-				//Log.i("COLISAO TIRO", "Esse eh contra  " + jogador.nome);
-				if(balaRect.intersect(jogador.getRect())){
-					jogador.collisionTiro(this, dano);
-					
+			{
+				if(balaRect.intersect(jogador.getRect()))
+				{
+					jogador.respawn();
 					Log.i("COLISAO TIRO", "TOMOU   " + posicaoTiro);
 					return true;
 				}
+			}
 		}
 		return false;
 
 	}
 
-	private boolean Intersect(JogadorServer player){
-		if(this.balaRect.centerX() > player.getRect().left && this.balaRect.centerX() < player.getRect().right &&
-				this.balaRect.centerY() > player.getRect().top && this.balaRect.centerY() < player.getRect().bottom) {
+	private boolean Intersect(JogadorServer player)
+	{
+		if(this.balaRect.intersect(player.getRect()))
 			return true;
-		}
+		
+//		
+//		if(this.balaRect.centerX() > player.getRect().left && this.balaRect.centerX() < player.getRect().right &&
+//				this.balaRect.centerY() > player.getRect().top && this.balaRect.centerY() < player.getRect().bottom) {
+//			return true;
+//		}
+		
 		return false;
 	}
-	public Point getPosition(){
+	public Point getPosition()
+	{
 		return new Point(balaRect.centerX(), balaRect.centerY());
 	}
-	public PlayerServer getOwner(){
+	
+	public PlayerServer getOwner()
+	{
 		return meuDono;
 	}
 	
-	private void setPosition(Point pos) {
+	private void setPosition(Point pos) 
+	{
 		int tempX = pos.x - this.balaRect.height()/2;
 		int tempY = pos.y - this.balaRect.width()/2;
 		
 		balaRect.set(tempX, tempY, pos.x + this.balaRect.height()/2, pos.y + this.balaRect.width()/2);
 	}
 
-	public Rect getRect() {
-		// TODO Auto-generated method stub
+	public Rect getRect() 
+	{
 		return balaRect;
 	}
 	
